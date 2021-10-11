4c23190a0be49dc7a55b2b71db12c88415440678
Sergey <invisibleone1992@gmail.com>
2021-10-11 22:36:00 +0300

    did tasks with flowers

--- lesson_002/08_garden.py
+++ lesson_002/08_garden.py
@@ -10,18 +10,26 @@ meadow = ('клевер', 'одуванчик', 'ромашка', 'клевер'
 # создайте множество цветов, произрастающих в саду и на лугу
 # garden_set =
 # meadow_set =
-# TODO здесь ваш код
+
+garden_set = set(garden)
+meadow_set = set(meadow)
 
 # выведите на консоль все виды цветов
-# TODO здесь ваш код
+
+flowers = garden_set | meadow_set
+print('Все виды цветов', flowers)
 
 # выведите на консоль те, которые растут и там и там
-# TODO здесь ваш код
+common_flowers = garden_set & meadow_set
+print('Цветы которые растут и там и там', common_flowers)
 
 # выведите на консоль те, которые растут в саду, но не растут на лугу
-# TODO здесь ваш код
+only_garden = garden_set.difference(meadow_set)
+print('Цветы только для сада', only_garden)
 
 # выведите на консоль те, которые растут на лугу, но не растут в саду
-# TODO здесь ваш код
+only_meadow = meadow_set.difference(garden_set)
+print('Цветы только для луга', only_meadow)
+
 
 
